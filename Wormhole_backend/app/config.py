28927812b1c311bec4b5f37f797e07b25677fb0b
zyxyxz <zrr-0518@163.com>
2025-12-31 13:51:57 +0800

    chore: configure super admin defaults

--- Wormhole_backend/app/config.py
+++ Wormhole_backend/app/config.py
@@ -14,8 +14,8 @@ class Settings(BaseSettings):
     COS_BUCKET: str = ""
     WECHAT_APP_ID: str = "wxf352f78176ea0dd2"
     WECHAT_APP_SECRET: str = "c4b2a446a0fc75c7f679ece1217be76b"
-    SUPER_ADMIN_OPENIDS: str = ""
-    SUPER_ADMIN_ROOM_CODE: str = ""
+    SUPER_ADMIN_OPENIDS: str = "oeGzH5XJLcl-i9K5XvkBS0g-mbec"
+    SUPER_ADMIN_ROOM_CODE: str = "201432"
 
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
